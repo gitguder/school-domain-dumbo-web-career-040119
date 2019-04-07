691ed25463ecda1e0ce85a6f850641ffb7f057d9
Codey Pinero <codey.pinero@gmail.com>
2019-04-07 03:23:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= [] 
+    roster[grade] << student
+    #binding.pry
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+    #binding.pry
+  end
+  
+  def sort
+    roster.each do |student_grade, students|
+      sorted = students.sort
+      roster[student_grade] = sorted
+      #binding.pry
+    end
+    
+  end
+  
+end
+
